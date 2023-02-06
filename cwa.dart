748a5a847f45d45fcaad31b93c34bb0febe12984
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-06 00:29:56 +0000

    Update calculation function

--- cwa.dart
+++ cwa.dart
@@ -2,35 +2,43 @@ import 'dart:io';
 
 void main() {
   int? numOfCourses;
-  int? creditHours;
   double cwa;
 
   print('How many courses are there?');
   numOfCourses = int.parse(stdin.readLineSync()!);
 
-  int cumulativeMarks = calculateMarks(numOfCourses);
-
-  print('Total credit hours');
-  creditHours = int.parse(stdin.readLineSync()!);
-
-  cwa = cumulativeMarks / creditHours;
+  cwa = calculateMarks(numOfCourses);
 
   print(cwa);
 }
 
-int calculateMarks(numOfCourses) {
+double calculateMarks(numOfCourses) {
   var marks = [];
+  var creditHours = [];
   int? score;
+  int? credit;
   int cumulativeMarks = 0;
 
-  print('Enter the marks');
-
   for (var i = 0; i < numOfCourses; i++) {
+    print('Mark:');
     score = int.parse(stdin.readLineSync()!);
-    marks.add(score);
+
+    print('Credit Hours');
+    credit = int.parse(stdin.readLineSync()!);
+    creditHours.add(credit);
+
+    marks.add(score * credit);
   }
+
+  int totalCredits = creditHours.reduce((a, b) => a + b);
 
   cumulativeMarks = marks.reduce((a, b) => a + b);
 
-  return cumulativeMarks;
+  print(cumulativeMarks);
+
+  return cumulativeMarks / totalCredits;
+}
+
+int totalCreditHours(creditHours) {
+  return 0;
 }
