d3e2f62eaf72f85b6b795e1d73f70b1b1857c388
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-11 21:16:39 +0000

    Move material app to new class

--- assignment/lib/main.dart
+++ assignment/lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Assignment(),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Assignment(),
+    );
+  }
 }
 
 class Assignment extends StatelessWidget {
