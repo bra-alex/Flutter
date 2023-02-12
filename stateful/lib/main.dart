175939d455e0c0e8f368b8110cb10bc57e794ee1
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-12 12:50:47 +0000

    Create login screen

---
+++ stateful/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            children: [
+              const Text(
+                'Please enter your email and password to login',
+                style: TextStyle(fontSize: 25),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                autocorrect: false,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                  // label: Row(children: const [
+                  //   Icon(Icons.email_outlined),
+                  //   Text('Email')
+                  // ]),
+                  border: OutlineInputBorder(),
+                  // icon: Icon(Icons.email_outlined)
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                obscureText: true,
+                autocorrect: false,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.password),
+                  suffix: Icon(Icons.remove_red_eye),
+                  border: OutlineInputBorder(),
+                  // icon: Icon(Icons.password),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                height: 60,
+                width: 200,
+                decoration: const BoxDecoration(color: Colors.blue),
+                child: const Center(
+                    child: Text('Bar',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold))),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
