c6d50d51097bce4ee397525350dcec3621f1b1c2
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-15 23:53:00 +0000

    Add textFields

--- assignment2/lib/login_screen.dart
+++ assignment2/lib/login_screen.dart
@@ -11,9 +11,79 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Backround(
+    return Backround(
+        child: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
         child: Scaffold(
-      backgroundColor: Colors.transparent,
+          backgroundColor: Colors.transparent,
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                'Login',
+                style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              TextInputs(placeholder: 'Email Address'),
+              SizedBox(
+                height: 40,
+              ),
+              TextInputs(placeholder: 'Password')
+            ],
+          ),
+        ),
+      ),
     ));
+  }
+}
+
+class TextInputs extends StatefulWidget {
+  const TextInputs({super.key, required this.placeholder});
+
+  final String placeholder;
+  @override
+  State<TextInputs> createState() => _TextInputsState();
+}
+
+class _TextInputsState extends State<TextInputs> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Material(
+          color: Colors.transparent,
+          elevation: 1.0,
+          shadowColor: Colors.black.withOpacity(0.5),
+          borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+          child: TextFormField(
+            style: const TextStyle(color: Colors.white),
+            keyboardType: TextInputType.emailAddress,
+            autocorrect: false,
+            decoration: InputDecoration(
+                fillColor: Colors.white.withOpacity(0.2),
+                filled: true,
+                label: Text(
+                  widget.placeholder,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+          ),
+        )
+      ],
+    );
   }
 }
