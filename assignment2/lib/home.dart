8588b241ad62de640772497858e515fddf9a7964
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-16 01:16:08 +0000

    add home screen

---
+++ assignment2/lib/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:assignment2/background.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Background(
+        child: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'App under construction...',
+              style: TextStyle(color: Colors.white, fontSize: 40),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
