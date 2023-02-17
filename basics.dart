a2b93cf1efac42c9d3c18f4adfdc6cc545a93749
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-17 07:50:09 +0000

    Add enumerations and switch statements

--- basics.dart
+++ basics.dart
@@ -10,6 +10,7 @@ maps(){
 
 // Null Safety
 // Null-aware assignment operator
+/*
 nullSafety(String? fName, String? mName, String? lName) {
   String? name = fName ?? mName ?? lName;
   print(name);
@@ -25,9 +26,35 @@ nullAssessment(List<String>? names) {
   final length = names?.length ?? 0;
   print(length);
 }
+*/
+
+// Enumerations
+// List of related items
+enum Weather { hot, cold, warm, humid }
+
+// Switch statements
+printWeather(Weather weather) {
+  switch (weather) {
+    case Weather.hot:
+      print('It is hot.');
+      break;
+    case Weather.cold:
+      print('It is cold.');
+      break;
+    case Weather.warm:
+      print('It is warm.');
+      break;
+    case Weather.humid:
+      print('It is humid.');
+      break;
+  }
+}
 
 void main() {
+  /*
   nullSafety(null, 'Asiamah', 'Boakye');
   nullSafetyAssignment(null, 'Asiamah', 'Boakye');
   nullAssessment(null);
+  */
+  printWeather(Weather.hot);
 }
