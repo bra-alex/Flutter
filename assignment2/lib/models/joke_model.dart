025f154bda5166d395104a5cd6972506e1d63799
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-20 08:12:46 +0000

    Update model

--- assignment2/lib/models/joke_model.dart
+++ assignment2/lib/models/joke_model.dart
@@ -9,4 +9,12 @@ class Jokes {
       required this.type,
       required this.setup,
       required this.punchline});
+
+  factory Jokes.fromJSON(Map<String, dynamic> json) {
+    return Jokes(
+        id: json['id'],
+        type: json['type'],
+        setup: json['setup'],
+        punchline: json['punchline']);
+  }
 }
