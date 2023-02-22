d9c17b4d74f2563ef52dcb15bd630ecfba9c5d51
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-22 13:36:15 +0000

    Add model

---
+++ activity/lib/model/activity_model.dart
@@ -0,0 +1,20 @@
+class Activity {
+  String activity;
+  String type;
+  int participants;
+  int price;
+
+  Activity(
+      {required this.activity,
+      required this.type,
+      required this.participants,
+      required this.price});
+
+  factory Activity.fromJSON(Map<String, dynamic> json) {
+    return Activity(
+        activity: json['activity'],
+        type: json['type'],
+        participants: json['participants'],
+        price: json['price']);
+  }
+}
