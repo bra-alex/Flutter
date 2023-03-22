2a16e4c1b8fdbc0e21403f35923a9f35b0bf41ed
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-03-22 20:28:05 +0000

    Add category meals page

---
+++ academind/food_app/lib/screens/category_meals_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  const CategoryMealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Category'),
+      ),
+      body: const Center(
+        child: Text('Recipes'),
+      ),
+    );
+  }
+}
