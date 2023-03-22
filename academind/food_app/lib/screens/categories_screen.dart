c990e31f61b403c38669bb855c94f91f26f08cc8
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-03-22 19:07:35 +0000

    Add categories screen

---
+++ academind/food_app/lib/screens/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/models/dummy_data.dart';
+import 'package:food_app/widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: dummyCategories.map((category) {
+          return CategoryItem(title: category.title, color: category.color);
+        }).toList(),
+      ),
+    );
+  }
+}
